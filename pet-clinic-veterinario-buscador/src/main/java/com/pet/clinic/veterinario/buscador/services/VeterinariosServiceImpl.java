41e2bfeb55f8f855346f465cc9044f6e89b8af6e
Camilo Martinez <camartinezm92@gmail.com>
2023-06-03 16:04:29 -0500

    Desarrollo para full stack

--- pet-clinic-veterinario-buscador/src/main/java/com/pet/clinic/veterinario/buscador/services/VeterinariosServiceImpl.java
+++ pet-clinic-veterinario-buscador/src/main/java/com/pet/clinic/veterinario/buscador/services/VeterinariosServiceImpl.java
@@ -1,43 +1,71 @@
 package com.pet.clinic.veterinario.buscador.services;
 
+import com.pet.clinic.buscador.models.dtos.PropietarioDto;
+import com.pet.clinic.veterinario.buscador.enums.ResponseMessageEnum;
+import com.pet.clinic.veterinario.buscador.models.dto.VeterinarioDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.pet.clinic.veterinario.buscador.pojos.ResponsePojo;
 import com.pet.clinic.veterinario.buscador.pojos.VeterinariosRequestPojo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class VeterinariosServiceImpl implements IVeterinarioService {
     
     @Autowired
     IVeterinarioService iVeterinarioRepository;
 
+	@Autowired
+	ResponsePojo responsePojo;
+
+	private List<VeterinarioDto> veterinarioDtos;
 	@Override
 	public ResponsePojo getVeterinario() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getVeterinario'");
+
+		this.veterinarioDtos = new ArrayList<>();
+		veterinarioDtos = iVeterinarioRepository.getVeterinario(); // pendiente repository
+		if (veterinarioDtos.isEmpty()) {
+			responsePojo.setMessages(ResponseMessageEnum.MESSAGE_ERROR_NOT_FOUND_ENUM.getMessages());
+		} else {
+			responsePojo.setMessages(ResponseMessageEnum.MESSAGE_OK_ENUM.getMessages());
+			responsePojo.setData(this.veterinarioDtos);
+		}
+	return responsePojo;
 	}
 
 	@Override
 	public ResponsePojo findVeterinarioById(Long veterinarioId) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'findVeterinarioById'");
+		this.veterinarioDtos = new LinkedList<>();
+		veterinarioDtos = iVeterinarioRepository.getVeterinario(veterinarioId); // pendiente repository
+		if (veterinarioDtos.isEmpty()) {
+			responsePojo.setMessages(ResponseMessageEnum.MESSAGE_ERROR_NOT_FOUND_ENUM.getMessages());
+		} else {
+			responsePojo.setMessages(ResponseMessageEnum.MESSAGE_OK_ENUM.getMessages());
+			responsePojo.setData(this.veterinarioDtos);
+		}
+		return responsePojo;
+		}
+
+
+		// revisar funcionamiento
+	@Override
+	public ResponsePojo saveVeterinario(VeterinariosRequestPojo VeterinariosRequestPojo) {
+		responsePojo.setMessages(ResponseMessageEnum.MESSAGE_OK_ENUM.getMessages());
+		return responsePojo;
 	}
 
 	@Override
-	public ResponsePojo saveVeterinario(VeterinariosRequestPojo veterinario) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'saveVeterinario'");
-	}
-
-	@Override
-	public ResponsePojo updateVeterinario(VeterinariosRequestPojo veterinario, Long venerarioId) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'updateVeterinario'");
+	public ResponsePojo updateVeterinario(VeterinariosRequestPojo VeterinariosRequestPojo, Long venerarioId) {
+		responsePojo.setMessages(ResponseMessageEnum.MESSAGE_OK_ENUM.getMessages());
+		return responsePojo;
 	}
 
 	@Override
 	public Boolean deleteVeterinario(Long veterinarioId) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'deleteVeterinario'");
+		responsePojo.setMessages(ResponseMessageEnum.MESSAGE_OK_ENUM.getMessages());
+		return Boolean.TRUE;
 	}
     
 }
